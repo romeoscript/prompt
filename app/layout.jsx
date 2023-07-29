92d334478f9e4e52337a43c0519059164bdb77d9
romeoscript <romeobourne211@gmail.com>
2023-07-29 08:29:26 +0100

    feat:first commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css'
+
+export const  metadata ={
+    title: 'promptopia',
+    description: 'discover my ai generated prompts ',
+}
+
+const RootLayout = ({children}) => {
+  return (
+   <html lang='en'>
+        <body>
+            <div className="main">
+                <div className="gradient"> </div>
+            </div>
+            <main className="app">
+                {children}
+            </main>
+        </body>
+   </html>
+  )
+}
+
+export default RootLayout
