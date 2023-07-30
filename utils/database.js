667d3ff326bf6c2ddedf43899cf5cb1140aed235
romeoscript <romeobourne211@gmail.com>
2023-07-30 21:03:03 +0100

    feat:fix bug login

--- utils/database.js
+++ utils/database.js
@@ -1,24 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let isConnected = true;
+let isConnected = false; // track the connection
 
-export const connectDB = async () => {
-  mongoose.set("strictQuery", true);
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
 
-  if (isConnected) {
-    console.log("MongoDB is connected to the database");
+  if(isConnected) {
+    console.log('MongoDB is already connected');
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-      useNewUriParser: true,
-      useUnifiedTripology: true,
-    });
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
     isConnected = true;
-    console.log("MongoDB is connected");
+
+    console.log('MongoDB connected')
   } catch (error) {
     console.log(error);
   }
-};
+}
